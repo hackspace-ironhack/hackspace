b2cf19b0e61155fd93785d31f50076d3a1d95e76
Juliana Ferreira Bastos <julferbas@yahoo.com.br>
2019-10-08 23:15:22 +0200

    added build files and added .env back to the .gitignore file

---
+++ client/build/precache-manifest.f568c1a02af56abdb86f656fb0c98f5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea0a91d4a02d6d94a44b396f024f82bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c195b8fa8c37e1c1c8c",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "4e80a40287e66aabd91f",
+    "url": "/static/css/main.d0f40f04.chunk.css"
+  },
+  {
+    "revision": "4c195b8fa8c37e1c1c8c",
+    "url": "/static/js/2.c86a889f.chunk.js"
+  },
+  {
+    "revision": "4e80a40287e66aabd91f",
+    "url": "/static/js/main.d090cb7b.chunk.js"
+  },
+  {
+    "revision": "7f45875b99a5ef02142c",
+    "url": "/static/js/runtime-main.c2763ce9.js"
+  }
+]);
