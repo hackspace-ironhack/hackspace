2194895ca37e694cce9c69532d7e7e733e98c4b3
Juliana Ferreira Bastos <julferbas@yahoo.com.br>
2019-10-09 00:03:17 +0200

    removed unused var

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App user={user} />
+    <App/>
   </BrowserRouter>,
   document.getElementById("root")
 );
